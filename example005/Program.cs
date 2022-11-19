342f865c641e02925cf069580c079db7d16073fa
Denis <denul73@gmail.com>
2022-11-19 20:31:55 +0300

    сделали программу приветствия

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура,это же Олька!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
